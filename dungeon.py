586e1429a08968b37cced5a3528447304219947b
Daniel Jenkins <daniel.jenkins@kantarworldpanel.com>
2015-12-09 14:32:05 +0000

    Completed basic dungeon game.

--- dungeon.py
+++ dungeon.py
@@ -1,47 +1,97 @@
 # Dungeon game for Python practice
-import random
 
-# Requirements:
-# - 
+import random
 
 CELLS = [(0, 0), (0, 1), (0, 2),
         (1, 0), (1, 1), (1, 2),
         (2, 0), (2, 1), (2, 2)]
 
 def get_locations():
-  # monster = random location
-  # door = random location
-  # start = random location
+  monster = random.choice(CELLS)
+  door = random.choice(CELLS)
+  start = random.choice(CELLS)
 
   # if monster, door or start are the same run the process again
+  if monster == door or door == start:
+    return get_locations()
 
-  # return monster, door, start
+  return monster, door, start
 
 def move_player(player, move):
   # get the player position
-  # if move is left, y - 1
-  # if move is right, y + 1
-  # if move is up, x - 1
-  # if move is down, x + 1
-  return player
+  player_x, player_y = player
+
+  if move == 'UP':
+    player_x -= 1
+  elif move == 'DOWN':
+    player_x += 1
+  elif move == 'LEFT':
+    player_y -= 1
+  elif move == 'RIGHT':
+    player_y += 1
+  
+  return = player_x, player_y
 
 def get_moves(player):
-  MOVES =['LEFT', 'RIGHT', 'UP', 'DOWN']
-  # if player's y is 0 remove LEFT
-  # if player's x is 0 remove UP
-  # if player's y is 2 remove RIGHT
-  # if player's x is 2 remove DOWN
-  return MOVES
+  moves =['LEFT', 'RIGHT', 'UP', 'DOWN']
+  # player = (x, y)
+  if player[0] == 0:
+    moves.remove('UP')
+  elif player[0] == 2:
+    moves.remove('DOWN')
+
+  if player[1] == 0:
+    moves.remove('LEFT')
+  elif player[1] == 2:
+    moves.remove('RIGHT')
+
+  return moves
+
+def draw_map(player):
+  print(" _ _ _ ")
+  tile = "|{}"
+  for idx, cell in enumerate(CELLS):
+    if idx in (0, 1, 3, 4, 6, 7):
+      if cell == player:
+        print(tile.format('X'), end="")
+      else:
+        print(tile.format('_'), end="")
+    else:
+      if cell == player:
+        print(tile.format('X|'))
+      else:
+        print(tile.format('_|'))
+
+monster, door, player = get_locations()
+print("Welome, welcome to dungeon!")
 
 while True:
-  print("Welome, welcome to dungeon!")
-  print("You're currently in room {}")  # fill in with player position
-  print("You can move {}")  # fill in with available moves
-  print("Enter 'LET ME OUT' to get out")
+  moves = get_moves(player)
+
+  print("You're currently in room {}.".format(player))
+
+  draw_map(player)
+
+  print("You can move {}".format(moves))  # fill in with available moves
+  print("Enter 'BEAM ME UP' to be teleported out")
 
   move = input("> ")
   move = move.upper()
-  if move == "LET ME OUT":
+  
+  if move == "BEAM ME UP":
+    break
+
+  if move in moves:
+    player = move_player(player, move)
+  else:
+    print("Walls are hard, stop walking into them")
+    continue
+
+  if player == door:
+    print("You've escaped!")
+    break
+  elif player == monster:
+    print("You've been gobbled up for breakfast!!")
     break
 
   # if it's a good move, change the player's position
